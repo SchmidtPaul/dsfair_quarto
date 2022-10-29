e0b37dba61d61f452c2f0f74208d922209f659f4
SchmidtPaul <schmidtpaul@hotmail.de>
2022-10-29 14:00:12 +0200

    Reorganizing

---
+++ helpersrc.R
@@ -0,0 +1,14 @@
+# make result chunks green via styles.css
+knitr::opts_chunk$set(
+  class.output = "custom-output"
+)
+
+# get raw github URL from local path
+get_url_from_path <- function(path) {
+  stringr::str_c(here::here("master"),
+                 stringr::str_remove(path, here::here())) %>%
+    stringr::str_replace(pattern = dirname(here::here()),
+                         replacement = "https://raw.githubusercontent.com/SchmidtPaul") %>%
+    stringr::str_c('# data is available online:\n','path <- "', ., '"') %>%
+    cat()
+}
