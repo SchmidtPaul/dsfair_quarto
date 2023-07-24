dfb0e63331059f39eaeb6a627ee8d2157bfb8240
SchmidtPaul <schmidtpaul@hotmail.de>
2023-07-24 21:05:57 +0200

    fixed callout blocks and broken links

--- src/custom-callout-addref.lua
+++ src/custom-callout-addref.lua
@@ -1,14 +1,14 @@
-local h2 = pandoc.Header(2, "Additional Resources (click to show)")
+local h2 = pandoc.Header(2, "Additional Resources")
 
 function Div(el)
-if quarto.doc.isFormat("html") then
-if el.classes:includes('custom-callout-addref') then
-local content = el.content
-table.insert(content, 1, h2)
-return pandoc.Div(
-  content,
-  {class="callout-tip", collapse='true'}
-)
-end
-end
+  if quarto.doc.isFormat("html") then
+    if el.classes:includes('custom-callout-tip') then
+      local content = el.content
+      table.insert(content, 1, h2)
+      return pandoc.Div(
+        content,
+        {class="callout-tip", collapse='true'}
+      )
+    end
+  end
 end 
--- src/helpersrc.R
+++ src/helpersrc.R
@@ -1,7 +1,5 @@
 # make result chunks green via styles.css
-knitr::opts_chunk$set(
-  class.output = "custom-output"
-)
+knitr::opts_chunk$set(class.output = "custom-output")
 
 # get raw github URL from local path
 get_url_from_path <- function(path) {
@@ -9,13 +7,13 @@ get_url_from_path <- function(path) {
                  stringr::str_remove(path, here::here())) %>%
     stringr::str_replace(pattern = dirname(here::here()),
                          replacement = "https://raw.githubusercontent.com/SchmidtPaul") %>%
-    stringr::str_c('# data is available online:\n','path <- "', ., '"') %>%
+    stringr::str_c('# data is available online:\n', 'path <- "', ., '"') %>%
     cat()
 }
 
 # get formatted p-values from anova
-getp <- function(ANOVA, term){
-  tab <- broom::tidy(ANOVA) %>% 
+getp <- function(ANOVA, term) {
+  tab <- broom::tidy(ANOVA) %>%
     mutate(p_val = insight::format_p(p.value, stars = TRUE))
   
   vec <- set_names(x = tab$p_val, nm = tab$term)
