9ddefa9c29a74a0507930246c6be15f75b47150d
SchmidtPaul <schmidtpaul@hotmail.de>
2023-11-08 20:47:27 +0100

    added agumented & alpha

--- src/helpersrc.R
+++ src/helpersrc.R
@@ -12,13 +12,21 @@ get_url_from_path <- function(path) {
 }
 
 # get formatted p-values from anova
-getp <- function(ANOVA, term) {
+getp <- function(ANOVA, term, escape_asterisks = TRUE) {
   tab <- broom::tidy(ANOVA) %>%
-    mutate(p_val = insight::format_p(p.value, stars = TRUE))
+    mutate(p_val = insight::format_p(p.value, stars = TRUE)) %>% 
+    suppressWarnings()
   
   vec <- set_names(x = tab$p_val, nm = tab$term)
   
-  vec[[term]]
+  # Check if asterisks should be escaped
+  if (escape_asterisks) {
+    # Escape asterisks
+    return(gsub("*", "\\*", vec[[term]], fixed = TRUE))
+  } else {
+    return(vec[[term]])
+  }
 }
+
 
 
